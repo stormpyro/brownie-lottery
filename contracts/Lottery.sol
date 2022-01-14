8ced9538ee2e90b54e79addb8d481500dc5a2d9b
Stormpyro <tatto1795@gmail.com>
2022-01-14 14:03:46 -0500

    Add enum to control Lottery state. Add onlyOwner modifier from openZeppelin

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18); // 18 decimals
@@ -15,6 +22,7 @@ contract Lottery {
 
     function enter() public payable {
         // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not Enough ETH!");
         players.push(msg.sender);
     }
@@ -36,7 +44,15 @@ contract Lottery {
 
     // Importante: Need a view function to convert from ETH to Wei
 
-    function startLottery() external {}
+    function startLottery() external onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {
+        
+    }
 }
